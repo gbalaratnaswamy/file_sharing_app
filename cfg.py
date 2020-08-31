759d7a70630de814c1ea599c1c13ece9d1c97b44
gbalaratnaswamy <gbalaratnaswamy@gmail.com>
2020-08-31 21:55:10 +0530

    requirements added

--- cfg.py
+++ cfg.py
@@ -2,8 +2,9 @@ DATABASE_NAME = "file_share"
 USER_COLLECTION = "user_basic_data"
 FILES_COLLECTION = "files"
 FILES_HISTORY = "files_downloads"
-AUTH_COLLECTION="auth_tokens"
+AUTH_COLLECTION = "auth_tokens"
 DATABASE_URL = "mongodb://localhost:27017/file_share."
 
+AUTH_COOKIE_DURATION = 30 * 24 * 60 * 60
 
 COOKIE_LENGTH = 10
--- cookies.py
+++ cookies.py
@@ -1,19 +1,20 @@
 import encryption
 from flask import make_response, redirect
+from cfg import AUTH_COOKIE_DURATION
 
 
 # create cookie token and add to database
 def create_auth_cookie(collection, request, redirect_url):
     response = make_response(redirect(redirect_url))
     random_string = encryption.create_cookie_auth(collection)
-    response.set_cookie("email", request.form["email"])
-    response.set_cookie("token", random_string)
+    response.set_cookie("email", request.form["email"], max_age=AUTH_COOKIE_DURATION)
+    response.set_cookie("token", random_string, max_age=AUTH_COOKIE_DURATION)
     collection.insert_one({"email": request.form["email"], "token": encryption.encrypt_string(random_string)})
     return response
 
 
 def clear_auth_cookies(collection, request):
-    response=make_response(redirect("/login"))
+    response = make_response(redirect("/login"))
     response.delete_cookie("email")
     token = request.cookies.get("token")
     response.delete_cookie("token")
